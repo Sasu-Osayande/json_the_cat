2fcea7b250e2807206da6d55462e6b44587d26ab
Sasu Osayande <osayande.sasu@outlook.com>
2022-02-17 17:31:36 -0500

    refactored to use the callback function

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,14 +1,17 @@
 const request = require("request");
 
-const args = process.argv.slice(2);
+const fetchBreedDescription = function(breedName, callback) {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, function(error, response, body) {
+    if (error) {
+      callback(error, null);
+    }
+    const data = JSON.parse(body);
+    if (data.length === 0) {
+      callback(null, "BREED NOT FOUND");
+    } else {
+      callback(null, data[0]["description"]);
+    }
+  });
+};
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${args}`, function(error, response, body) {
-  if (error) {
-    return console.error("Error:", error);
-  }
-  const data = JSON.parse(body);
-  if (data.length === 0) {
-    return console.log("BREED NOT FOUND");
-  }
-  console.log(data[0]["description"]);
-});
+module.exports = { fetchBreedDescription };
